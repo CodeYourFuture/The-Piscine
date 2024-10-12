16a2fafbc98ced5a2fc080b3fcac7b6edb0863cf
Daniel Wagner-Hall <dawagner@gmail.com>
2024-10-12 23:59:55 +0100

    Add Days Calendar project

---
+++ Project-Days-Calendar/common.mjs
@@ -0,0 +1,5 @@
+// This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script.
+
+export function getGreeting() {
+    return "Hello";
+}
