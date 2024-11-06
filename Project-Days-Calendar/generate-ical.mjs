135763d47183a64ba10b0048a9ec280063d5799e
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2024-11-06 16:16:52 +0000

    Update Project-Days-Calendar/generate-ical.mjs
    Co-authored-by: Alasdair Smith <alasdairsmith100@gmail.com>

--- Project-Days-Calendar/generate-ical.mjs
+++ Project-Days-Calendar/generate-ical.mjs
@@ -1,4 +1,4 @@
-// This is a placeholder file which shows how you can access functions and data defined in other files.
+// This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
 // It can be run with `node`.
 
 import { getGreeting } from "./common.mjs";
