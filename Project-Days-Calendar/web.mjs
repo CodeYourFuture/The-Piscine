348461f245860e5312c73c34f3607f1a996b0852
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2024-11-06 16:17:41 +0000

    Update Project-Days-Calendar/web.mjs
    Co-authored-by: Alasdair Smith <alasdairsmith100@gmail.com>

--- Project-Days-Calendar/web.mjs
+++ Project-Days-Calendar/web.mjs
@@ -7,5 +7,5 @@ import { getGreeting } from "./common.mjs";
 import daysData from "../days.json" with { type: "json" };
 
 window.onload = function() {
-    document.getElementsByTagName("body")[0].innerText = `${getGreeting()} - there are ${daysData.length} know days`;
+    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} know days`;
 }
