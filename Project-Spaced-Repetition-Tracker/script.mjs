dea89d75682222b6c7393b98824a7c730dff4561
Faithy4444 <faithy4444@gmail.com>
2025-06-08 21:48:21 +0200

    function to render the topic and date of a user

--- Project-Spaced-Repetition-Tracker/script.mjs
+++ Project-Spaced-Repetition-Tracker/script.mjs
@@ -3,9 +3,10 @@
 // You can delete the contents of the file once you have understood how it works.
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
-
-// needed variables
+ 
 import { getUserIDs } from "./common.mjs";
+import { addData } from "./storage.mjs";
+import { getData } from "./storage.mjs";
 
 window.onload = function () {
   const users = getUserIDs();
@@ -13,6 +14,7 @@ window.onload = function () {
   let userDropdown = document.getElementById('dropdown')
   populateDropdown(users, userDropdown)
 };
+
 
 function populateDropdown(users, userDropdown) {
   if (!userDropdown) return;
@@ -24,3 +26,35 @@ function populateDropdown(users, userDropdown) {
     userDropdown.appendChild(option);
   });
 }
+
+
+
+/*let newData = [
+  {
+    userId: 1,
+    topic: "the great trek",
+    date: "2025/03/31"
+  },
+  {
+    userId: 1,
+    topic: "the great trek",
+    date: "2025/03/31"
+  }
+]*/
+//This function displays the agendas for the selected user. This function will run only when a user is selected
+let agendaContainer = document.getElementById("agendas")
+function renderAgenda(Userdata) {
+
+  let agendaList = document.createElement("li")
+  Userdata.forEach(user => {
+    let listItem = document.createElement("ul")
+    listItem.textContent = `${user.topic} start date:${user.date}`
+
+    agendaList.appendChild(listItem)
+  });
+  
+  agendaContainer.appendChild(agendaList)
+  
+ 
+}
+
