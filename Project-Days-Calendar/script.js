218f036d245bdad8d76fa2a684bd8f147bcb420e
Roman <roman.boxes@gmail.com>
2026-02-23 18:35:46 +0100

    feat (function renderCalendar (month, year) and function renderDay(dayNumber)
    - made months array - for having the name of the month not it's index
    - made days of the week array - for having the name of the day of the week not it's index
    renderCalendar function :
    
    Takes the year and month as parameters
    - clean everything inside the section
    - create div and h5 with names of the week and rendering them
    - create variable to check what day of the week the first day of the month is.
    - create variable for length of the month
    - for now create emply div for days before the first day of the month
    - rendering days of the month
    
    renderDay function:
    - for now create buttons for each day of the month
    - add class days
    - add text with the number  and rendering them

--- Project-Days-Calendar/script.js
+++ Project-Days-Calendar/script.js
@@ -1,12 +1,44 @@
+const months = [
+  "January", "February", "March", "April",
+  "May", "June", "July", "August",
+  "September", "October", "November", "December"
+];
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const root = document.querySelector(".week-section");
+
 const currentDate = new Date();
-let day = currentDate.getDay();
+const currentDay = currentDate.getDay();
 const currentMonth = currentDate.getMonth();
 const currentYear = currentDate.getFullYear();
-console.log(currentDate)
-console.log(currentMonth)
-console.log(currentYear)
-console.log(day)
 
 
-const d = new Date("April 23, 2015 01:15:00");
-console.log(d.getDay())
+
+renderCalendar(currentMonth, currentYear);
+
+function renderCalendar(month, year) {
+    root.innerHTML = "";
+    for (let i = 0; i < daysOfTheWeek.length; i++) {
+        const dayOfTheWeek = document.createElement("div");
+        const header = document.createElement("h5");
+        header.textContent = daysOfTheWeek[i];        
+        root.appendChild(dayOfTheWeek);
+        dayOfTheWeek.appendChild(header);
+    }        
+    const firstDayIndex = new Date(year, month, 1).getDay();    
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    for (let i = 0; i < firstDayIndex; i++) {
+        const emptyCell = document.createElement("div");     
+        root.appendChild(emptyCell)
+    }
+    for (let day = 1; day <= daysInMonth; day++) {
+        renderDay(day);
+    }
+}
+
+function renderDay(dayNumber) {
+    const dayCard = document.createElement("button");
+    dayCard.classList.add("days");
+    dayCard.textContent = dayNumber;
+    root.appendChild(dayCard);
+}
