60d8085b5b3dcae88390fa4d5d60da1081726236
Roman <roman.boxes@gmail.com>
2026-02-23 22:50:28 +0100

    feat (renderMonth / today is selected / next and previous month
    renderMonth is show current month
    - if today is matching with some day of selected month it will be showed
    - button next now change the current month and show next month
    - button previous now change the current month and show previous month
    CSS
    - for today some different style
    - class for days of the week
    HTML
    - the month-value now dynamic and buttons previous/next month

--- Project-Days-Calendar/script.js
+++ Project-Days-Calendar/script.js
@@ -9,30 +9,38 @@ const root = document.querySelector(".week-section");
 
 const currentDate = new Date();
 const currentDay = currentDate.getDay();
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
+let currentMonth = new Date().getMonth();
+let currentYear = new Date().getFullYear();
 
 
-
+renderMonth(currentMonth)
 renderCalendar(currentMonth, currentYear);
 
 function renderCalendar(month, year) {
     root.innerHTML = "";
+    document.querySelector("h1").textContent = months[month];
+
     for (let i = 0; i < daysOfTheWeek.length; i++) {
         const dayOfTheWeek = document.createElement("div");
         const header = document.createElement("h5");
+        header.classList.add("dayOfTheWeek")
         header.textContent = daysOfTheWeek[i];        
         root.appendChild(dayOfTheWeek);
         dayOfTheWeek.appendChild(header);
     }        
     const firstDayIndex = new Date(year, month, 1).getDay();    
     const daysInMonth = new Date(year, month + 1, 0).getDate();
+
     for (let i = 0; i < firstDayIndex; i++) {
         const emptyCell = document.createElement("div");     
         root.appendChild(emptyCell)
     }
     for (let day = 1; day <= daysInMonth; day++) {
-        renderDay(day);
+        const dayCard = renderDay(day); 
+        if (day === currentDate.getDate() && year === currentDate.getFullYear() && month === currentDate.getMonth()) {
+            dayCard.classList.add("today");
+        }
+        root.appendChild(dayCard);
     }
 }
 
@@ -41,4 +49,35 @@ function renderDay(dayNumber) {
     dayCard.classList.add("days");
     dayCard.textContent = dayNumber;
     root.appendChild(dayCard);
+    return dayCard;
 }
+function renderMonth(month) {
+    const shownMonth = document.querySelector("h1");
+    shownMonth.textContent = months[month];
+}
+
+
+
+const nextBtn = document.querySelector(".next-month");
+nextBtn.addEventListener("click", () => {
+        currentMonth++;
+
+    if (currentMonth > 11) {
+        currentMonth = 0;
+        currentYear++;
+    }
+
+    renderCalendar(currentMonth, currentYear);
+});
+
+const previousMonth = document.querySelector(".previous-month");
+previousMonth.addEventListener("click", () => {
+    currentMonth--;
+
+    if (currentMonth < 0) {
+        currentMonth = 11;
+        currentYear--;
+    }
+
+    renderCalendar(currentMonth, currentYear);
+});
