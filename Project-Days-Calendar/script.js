232d74865b9241f2f4d139bdd69add4823826352
Roman <roman.boxes@gmail.com>
2026-02-23 11:44:24 +0100

    create script.js and style.css / fix generate-ical.mjs  / uptate index.html
    create script.js
    - just test getDay(), new Date()  ... methods
    style.css :
    -  display: grid
    - basic structure
    fix generate-ical.mjs
    update index.html to merch with dynamic structure in the future

---
+++ Project-Days-Calendar/script.js
@@ -0,0 +1,12 @@
+const currentDate = new Date();
+let day = currentDate.getDay();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+console.log(currentDate)
+console.log(currentMonth)
+console.log(currentYear)
+console.log(day)
+
+
+const d = new Date("April 23, 2015 01:15:00");
+console.log(d.getDay())
