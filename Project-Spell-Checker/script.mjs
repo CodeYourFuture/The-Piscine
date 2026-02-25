2bf16d9144e5d9aab5d632fc0f9607ed7e6ac688
Roman <roman.boxes@gmail.com>
2026-02-25 12:14:32 +0100

    basic structure and fix the import
    Create a basic structure for a spell checker

--- Project-Spell-Checker/script.mjs
+++ Project-Spell-Checker/script.mjs
@@ -4,8 +4,21 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import getDictionarySize from "./common.mjs";
+import { getDictionarySize } from "./common.mjs";
 
-window.onload = function() {
-    document.querySelector("body").innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
+window.onload = function () {
+    const body = document.querySelector("body")
+    body.innerText = `There are ${getDictionarySize()} words in the Basic English dictionary`;
+    const textSection = document.createElement("section");
+    body.append(textSection);
+    const subTitle = document.createElement("h2");
+    subTitle.textContent = "Check your text";
+    textSection.append(subTitle);
+    const text = document.createElement("textarea"); 
+    text.placeholder = "Put you text here if you want to check it";
+    textSection.append(text);
+    const checkBtn = document.createElement("button");
+    checkBtn.textContent = "check";
+    textSection.append(checkBtn);
+
 }
