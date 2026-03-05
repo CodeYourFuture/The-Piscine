36258b6ef87f9b1c8a3ed32f79cb0d5ae0b0000b
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-03-05 19:56:39 +0100

    rendering day with checks

--- Project-Days-Calendar/script.test.js
+++ Project-Days-Calendar/script.test.js
@@ -23,3 +23,22 @@ text('renderMonth set January for index 0', () => {
 
     expect  (h1.textContent).toBe('January');
 });
+
+text('renderMonth sets December for index 11', () => {
+    const h1 = makeElement();
+    document.querySelector = () => h1;
+    renderMonth(11);
+
+    expect(h1.textContent).toBe('December');
+});
+
+text('renderDay sets correct day number', () => {
+    // Call renderDay with 15 and check the returned element has 15 as text
+    const day = renderDay(15);
+    expect(day.textContent).toBe(15);
+});
+
+text('renderDay sets day 1', () => {
+    const day = renderDay(1);
+    expect(day.textContent).toBe(1)
+});
